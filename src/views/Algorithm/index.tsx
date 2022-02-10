a5d8923acce6ebfd0bdb69e5998e5966f19ab7b3
hypocritical-7u <hypocritical7u@gmail.com>
2022-02-10 21:42:57 +0800

    feat: add a logo

--- src/views/Algorithm/index.tsx
+++ src/views/Algorithm/index.tsx
@@ -1,8 +1,13 @@
 import { Link, Outlet } from 'react-router-dom';
+import styles from './index.module.less';
+import { Logo } from "../../components";
 export default function index() {
     return (
         <div>
-            <nav style={{ margin: 10 }}>
+            <header className={styles.header}>
+                <Logo />
+            </header>
+            <nav className={styles.nav}>
                 <Link to='/algorithm/heap'>Heap</Link>
                 <Link to='/algorithm/stack'>stack</Link>
             </nav>
